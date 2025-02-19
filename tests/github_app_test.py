ff1386a216e4a0cf420c1a16020e038d5a65f14e
Sviatoslav Sydorenko <webknjaz@redhat.com>
2025-02-19 15:38:19 +0100

    🧪 Rewrite GH auth test via real RSA keys and JWT
    This reduces the level of patching and moves most testing towards
    public API side effects.

--- tests/github_app_test.py
+++ tests/github_app_test.py
@@ -5,9 +5,76 @@ from typing import TypedDict
 import pytest
 from pytest_mock import MockerFixture
 
-from github.Auth import AppAuth, AppInstallationAuth
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric.rsa import (
+    RSAPrivateKey,
+    RSAPublicKey,
+    generate_private_key,
+)
+from cryptography.hazmat.primitives.serialization import (
+    Encoding,
+    NoEncryption,
+    PrivateFormat,
+    PublicFormat,
+)
+from github.Auth import AppInstallationAuth
+from github.Consts import DEFAULT_JWT_ALGORITHM
+from jwt import decode as decode_jwt
 
 from awx_plugins.credentials import github_app as gh_app_plugin_mod
+
+
+RSA_PUBLIC_EXPONENT = 65_537  # noqa: WPS303
+MINIMUM_RSA_KEY_SIZE = 1024  # the lowest value chosen for performance in tests
+
+
+@pytest.fixture(scope='module')
+def rsa_private_key() -> RSAPrivateKey:
+    """Generate an RSA private key."""
+    return generate_private_key(
+        public_exponent=RSA_PUBLIC_EXPONENT,
+        key_size=MINIMUM_RSA_KEY_SIZE,  # would be 4096 or higher in production
+        backend=default_backend(),
+    )
+
+
+@pytest.fixture(scope='module')
+def rsa_public_key(rsa_private_key: RSAPrivateKey) -> RSAPublicKey:
+    """Extract a public key out of the private one."""
+    return rsa_private_key.public_key()
+
+
+@pytest.fixture(scope='module')
+def rsa_private_key_bytes(rsa_private_key: RSAPrivateKey) -> bytes:
+    r"""Generate an unencrypted PKCS#1 formatted RSA private key.
+
+    Encoded as PEM-bytes.
+
+    This is what the GitHub-downloaded PEM files contain.
+
+    Ref: https://developer.github.com/apps/building-github-apps/\
+         authenticating-with-github-apps/
+    """
+    return rsa_private_key.private_bytes(
+        encoding=Encoding.PEM,
+        format=PrivateFormat.TraditionalOpenSSL,  # A.K.A. PKCS#1
+        encryption_algorithm=NoEncryption(),
+    )
+
+
+@pytest.fixture(scope='module')
+def rsa_private_key_str(rsa_private_key_bytes: bytes) -> str:
+    """Return private key as an instance of string."""
+    return rsa_private_key_bytes.decode('utf-8')
+
+
+@pytest.fixture(scope='module')
+def rsa_public_key_bytes(rsa_public_key: RSAPublicKey) -> bytes:
+    """Return a PKCS#1 formatted RSA public key encoded as PEM."""
+    return rsa_public_key.public_bytes(
+        encoding=Encoding.PEM,
+        format=PublicFormat.PKCS1,
+    )
 
 
 class AppInstallIds(TypedDict):
@@ -108,9 +175,24 @@ class _FakeAppInstallationAuth(AppInstallationAuth):
         return 'token-sentinel'
 
 
-def test_github_app_github_authentication(
+@pytest.mark.parametrize(
+    'application_id',
+    (123, '123', 'Iv1.aaaaaaaaaaaaaaaa'),
+    ids=('app-id-int', 'app-id-str', 'client-id'),
+)
+@pytest.mark.parametrize(
+    'installation_id',
+    (456, '456'),
+    ids=('install-id-int', 'install-id-str'),
+)
+# pylint: disable-next=too-many-arguments,too-many-positional-arguments
+def test_github_app_github_authentication(  # noqa: WPS211
+    application_id: int | str,
+    installation_id: int | str,
     mocker: MockerFixture,
     monkeypatch: pytest.MonkeyPatch,
+    rsa_private_key_str: str,
+    rsa_public_key_bytes: bytes,
 ) -> None:
     """Test successful GitHub authentication."""
     monkeypatch.setattr(
@@ -127,32 +209,41 @@ def test_github_app_github_authentication(
 
     token = gh_app_plugin_mod.extract_github_app_install_token(
         github_api_url='https://github.com',
-        app_id='123',
-        install_id='456',
-        private_rsa_key='example-key',
+        app_id=application_id,
+        install_id=installation_id,
+        private_rsa_key=rsa_private_key_str,
     )
+
+    observed_pygithub_obj = github_initializer_spy.spy_return
+    observed_gh_install_auth_obj = get_installation_auth_spy.spy_return
+    # pylint: disable-next=protected-access
+    signed_jwt = observed_gh_install_auth_obj._app_auth.token  # noqa: WPS437
 
     assert token == 'token-sentinel'
 
-    get_installation_auth_spy.assert_called_once_with(
-        mocker.ANY,
-        456,  # noqa: WPS432
-        None,
-        None,
-    )
-    first_arg_to_get_installation_auth = (
-        get_installation_auth_spy.
-        call_args[0][0]
-    )
-    assert isinstance(first_arg_to_get_installation_auth, AppAuth)
-    assert first_arg_to_get_installation_auth.app_id == 123  # noqa: WPS432
-    assert first_arg_to_get_installation_auth.private_key == 'example-key'
+    assert observed_pygithub_obj.requester.base_url == 'https://github.com'
 
-    github_initializer_spy.assert_called_once_with(
-        auth=mocker.ANY,
-        base_url='https://github.com',
-    )
-    assert isinstance(
-        github_initializer_spy.call_args[1]['auth'],
-        _FakeAppInstallationAuth,
+    assert observed_gh_install_auth_obj.installation_id == int(installation_id)
+    assert isinstance(observed_gh_install_auth_obj, _FakeAppInstallationAuth)
+
+    # NOTE: The `decode_jwt()` call asserts that no
+    # NOTE: `jwt.exceptions.InvalidSignatureError()` exception gets raised
+    # NOTE: which would indicate incorrect RSA key or corrupted payload if
+    # NOTE: that was to happen. This verifies that JWT is signed with the
+    # NOTE: private RSA key we passed by using its public counterpart.
+    decode_jwt(
+        signed_jwt,
+        key=rsa_public_key_bytes,
+        algorithms=[DEFAULT_JWT_ALGORITHM],
+        options={
+            'require': ['exp', 'iat', 'iss'],
+            'strict_aud': False,
+            'verify_aud': True,
+            'verify_exp': True,
+            'verify_signature': True,
+            'verify_nbf': True,
+        },
+        audience=None,  # GH App JWT don't set the audience claim
+        issuer=str(application_id),
+        leeway=0.001,  # noqa: WPS432
     )
